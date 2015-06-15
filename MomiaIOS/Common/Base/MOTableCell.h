e9a1a6bc7c53eff5ee9628d46467a379ef26c194
OwenLwf <owen.lwf@foxmail.com>
2015-06-15 10:39:59 +0800

    MOTableCell准备作为cell一个公共父类

---
+++ MomiaIOS/Common/Base/MOTableCell.h
@@ -0,0 +1,18 @@
+//
+//  MOTableCell.h
+//  MomiaIOS
+//
+//  Created by Owen on 15/6/15.
+//  Copyright (c) 2015年 Deng Jun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MOTableCell : UITableViewCell
+
+//+(instancetype)cellWithTableView:(UITableView *)tableView forIndexPath:(NSIndexPath *)indexPath withIdentifier:(NSString *) identifier;
+//
+//+(void)registerCellWithTableView:(UITableView *)tableView withIdentifier:(NSString *)identifier;
+
+
+@end
---
+++ MomiaIOS/Common/Base/MOTableCell.m
@@ -0,0 +1,34 @@
+//
+//  MOTableCell.m
+//  MomiaIOS
+//
+//  Created by Owen on 15/6/15.
+//  Copyright (c) 2015年 Deng Jun. All rights reserved.
+//
+
+#import "MOTableCell.h"
+
+@implementation MOTableCell
+
+//+(instancetype)cellWithTableView:(UITableView *)tableView forIndexPath:(NSIndexPath *)indexPath withIdentifier:(NSString *) identifier
+//{
+//    
+//}
+//
+//+(void)registerCellWithTableView:(UITableView *)tableView withIdentifier:(NSString *)identifier
+//{
+//    
+//}
+
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
