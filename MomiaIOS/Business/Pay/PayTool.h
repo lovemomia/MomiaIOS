49d01a417318f544221541544ea650ab88b98544
Deng Jun <dengjun86@gmail.com>
2015-07-09 16:06:26 +0800

    添加支付宝支付

---
+++ MomiaIOS/Business/Pay/PayTool.h
@@ -0,0 +1,18 @@
+//
+//  PayTool.h
+//  MomiaIOS
+//
+//  支付工具（对第三方支付的封装）
+//
+//  Created by Deng Jun on 15/7/9.
+//  Copyright (c) 2015年 Deng Jun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AlipaySDK/AlipaySDK.h>
+
+@interface PayTool : NSObject
+
+- (void)startAlipay;
+
+@end
