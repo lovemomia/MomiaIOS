bc9b9a4898afbdb6d6672d7528a27b068a31427f
pzzls <moshenglei@icloud.com>
2016-06-23 16:54:59 +0800

    add http request

--- ReactComponent/Common.js
+++ ReactComponent/Common.js
@@ -4,7 +4,21 @@ import React, {
 	Component
 } from 'react';
 
+var ReactNative = require('react-native');
 
+var {
+	View,
+	ActivityIndicatorIOS
+} = ReactNative;
+
+var SGStyles = require('./SGStyles');
+
+var styles = ReactNative.StyleSheet.create({
+  loadingContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 class Common extends React.Component {
 
@@ -16,7 +30,16 @@ class Common extends React.Component {
 	static domain() {
 		return Common.debug() ? 'http://i.momia.cn' : 'http://i.sogokids.com';
 	}
-}
 
+	static loading() {
+
+		return (
+			<View style={[SGStyles.container, styles.loadingContainer]}>
+				<ActivityIndicatorIOS hidden='false'
+      								  size='small'/>
+      		</View>
+		);
+	}
+}
 
 module.exports = Common;
--- ReactComponent/wenda/wdcourseintro.js
+++ ReactComponent/wenda/wdcourseintro.js
@@ -3,6 +3,10 @@
 var React = require('react');
 var ReactNative = require('react-native');
 
+var Common = require('../Common');
+var SGStyles = require('../SGStyles');
+var HttpService = require('../HttpService');
+
 var {
 	View,
 	Text,
@@ -23,11 +27,29 @@ var ds = new ListView.DataSource({
 
 var WendaCourseIntroComponent = React.createClass({
 
+	componentDidMount: function() {
+
+		console.log("============================");
+		 HttpService.get(Common.domain() + '/v1/wd_course?', {
+     	 	wid: 1,
+    	}, (resp) => {
+      		if (resp.errno == 0) {
+        		this._handlerResponse(resp.data);
+     	 	} else {
+        		// request failed
+        		this.setState({
+          		isLoading: false
+        	});
+     	}
+      		// console.log(resp.data);
+    	});
+	},
+
 	getInitialState: function() {
 		return {
-			dataSource: ds.cloneWithRows([
-				{id: 1},{id: 2},{id: 3},{id: 4}
-			])
+			dataSource: ds.cloneWithRows([new Array()]),
+			isLoading: true,
+			wdcourse: []
 		};
 	},
 
@@ -42,62 +64,95 @@ var WendaCourseIntroComponent = React.createClass({
 		);
 	},
 
-	_renderRow: function(rowData,sectionID,rowID) {
+	_handlerResponse: function(data) {
 
-		if (rowID == 0) {
-			return this._renderHeader();
-		} else if (rowID == 1) {
-			return this._renderTextView();
-		} else if (rowID ==2) {
-			return this._renderImage();
-		} else {
-			return this._renderTextView();
-		}
-		return (
-			<TouchableHighlight
-				onPress = {() => this._pressRow(rowData,sectionID,rowID)} >
-				<View>
-					<Text>Hello ,bog</Text>
-				</View>
-			</TouchableHighlight>
-		);
+		// console.log("hello");
+		let list = new Array();
+
+		list.push({
+			type: 1
+		});
+
+		list.push({
+			type: 2
+		});
+
+		list.push({
+			type: 3
+		});
+
+		list.push({
+			type: 4
+		});
+
+		this.setState({
+      		isLoading: false,
+      		dataSource: this.state.dataSource.cloneWithRows(list),
+      		wdcourse: data.wdcourse,
+    	});
+    	// console.log(list);
 	},
 
-	_renderHeader: function() {
+	_renderRow: function(rowData,sectionID,rowID) {
+
+		// console.log('-----' + rowData);
+		if (rowData.type == 1) {
+			return this._renderCourse();
+		} else if (rowData.type == 2) {
+			return this._renderText();
+		} else if (rowData.type == 3) {
+			return this._renderImage();
+		} else {
+			return this._renderText();
+		}
+	},
+
+	_renderCourse: function() {
+		data = this.state.wdcourse;
 		return (
+			<TouchableHighlight
+				onPress={() => this._pressRowItem()}
+				underlayColor = '#f1f1f1'>
 			<View style={{padding: 10,marginTop: 10,backgroundColor: 'white'}}>
 				<View style={{flexDirection: 'row',alignItems: 'center'}}>
 					<View>
-						<Image style={{width: 40,height: 40, backgroundColor: 'red'}}/>
+						<Image style={{width: 40,height: 40, backgroundColor: 'red'}}
+							   source={{uri: data.cover}}/>
 					</View>
 					<View style={{marginLeft: 10,flex: 1}}>
-						<Text style={{fontSize: 13}}>在孩子教育过程中，如何做到零吼叫？</Text>
+						<Text style={{fontSize: 13}}>{data.title}</Text>
 						<View style={{flexDirection: 'row',alignItems: 'center'}}>
+							<Image style={{width: 20,height: 20, backgroundColor: 'red'}}
+								   />
+							<Text style={{fontSize: 11, color: '#999999'}}>{data.count}次</Text>
 							<Image style={{width: 20,height: 20, backgroundColor: 'red'}}/>
-							<Text style={{fontSize: 11, color: '#999999'}}>20000次</Text>
-							<Image style={{width: 20,height: 20, backgroundColor: 'red'}}/>
-							<Text style={{fontSize: 11, color: '#999999'}}> 20分钟</Text>
+							<Text style={{fontSize: 11, color: '#999999'}}> {data.mins}分钟</Text>
 						</View>
-						<Text style={{fontSize: 11, color: '#999999'}}>2016年5月1日</Text>
+						<Text style={{fontSize: 11, color: '#999999'}}>{data.startTime}</Text>
 					</View>
 					<Image style={{width: 10,height: 10,backgroundColor: 'green'}}/>
 				</View>
 			</View>
+			</TouchableHighlight>
 		);
 	},
 
 	_renderImage: function() {
+		data = this.state.wdcourse;
 		return (
 			<View style={{padding: 10}}>
-				<Image style={{height: 120,backgroundColor: 'green'}} />
+				<Image style={{height: 120,backgroundColor: 'green'}}
+					   source={{uri:data.cover}} />
 			</View>
 		);
 	},
 
-	_renderTextView: function() {
+	_renderText: function() {
+
+		data = this.state.wdcourse;
 		return (
 			<View style={{padding: 10}}>
-				<Text>在讨伐克罗诺斯时，独眼巨人送给宙斯闪电火，给波塞冬三叉戟，给哈迪斯黑暗头盔。此后波塞冬经常手持三叉戟，这成了他的标志。当他愤怒时海底就会出现怪物，他挥动三叉戟不但能轻易掀起滔天巨浪引起风暴和海啸、使大陆沉没、天地崩裂，还能将万物打得粉碎，甚至引发震撼整个世界的强大地震，由于过于强悍，就连冥王都惧怕会不会令宇宙裂开导致冥界暴露在人间，但象征他的圣兽海豚则显示出海的宁静和波塞冬亲切的神性。爱琴海附近的希腊海员和渔民对他极为的崇拜</Text>
+				<Text>{data.desc}</Text>
 			</View>
 		);
 	},
